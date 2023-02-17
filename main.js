3e21780ce9d8a615ec6af0ae326a67f874e1f1f1
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-02-16 22:27:40 -0500

    Operators work as expected(visually)

--- main.js
+++ main.js
@@ -1,72 +1,89 @@
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
     const numBtns = document.querySelectorAll(".number");
     const operatorBtns = document.querySelectorAll(".operator")
-    
+
     const addBtn = document.querySelector("#plus");
     const subtractBtn = document.querySelector("#minus");
     const multiplyBtn = document.querySelector("#multiply");
     const divideBtn = document.querySelector("#divide");
-    
-    
+
+
 
 
     const plusbtn = document.querySelector("#plus");
 
     let currentStringValue = ""
     let currentNumValue = 0;
-    let previousNumValue =0;
+    let previousNumValue = 0;
 
     const currentValueView = document.querySelector(".currentValue")
 
     operatorPressed = false;
-    
 
-    
 
-    
 
-    for(let i = 0; i < numBtns.length;i++){
-        numBtns[i].addEventListener("click",()=>{
-            let pressedNum =numBtns[i].value
-            if(!operatorPressed){
-                currentStringValue+=pressedNum;
-                updateUIcurrentValue(pressedNum)
-            }else{
-                updateUIcurrentValue(pressedNum)
+
+
+
+    for (let i = 0; i < numBtns.length; i++) {
+        numBtns[i].addEventListener("click", () => {
+            let pressedNum = numBtns[i].value
+            if (!operatorPressed) {
+                currentStringValue = currentValueView.innerHTML;
+                currentStringValue += pressedNum;
+
+                currentValueView.innerHTML = currentStringValue;
+
+
+            } else {
+                currentNumValue = Number(currentStringValue);
+                previousNumValue = currentNumValue;
+                currentNumValue = 0;
+                currentValueView.innerHTML = pressedNum;
+                operatorPressed = false;
+
 
             }
-            
+
         })
     }
 
-    for(let i=0; i<operatorBtns.length;i++){
-        operatorBtns[i].addEventListener("click",()=>{
-            previousNumValue = currentNumValue;
+    for (let i = 0; i < operatorBtns.length; i++) {
+        operatorBtns[i].addEventListener("click", () => {
+
+            // previousNumValue = currentNumValue;
             operatorPressed = true;
 
         })
     }
 
-    function updateUIcurrentValue(updatedValye){
-        currentValueView.innerHTML = updatedValye;
-        currentNumValue = Number(currentStringValue)
+    function updateUIcurrentValue(updatedValue, resetNumber) {
+
+        if (resetNumber) {
+            previousNumValue = currentNumValue;
+            currentValueView.innerHTML = "";
+        } else {
+            currentValueView.innerHTML = currentStringValue;
+            currentNumValue = Number(currentStringValue)
+        }
+
     }
 
-    function add(a,b){
-        return a+b;
+    function add(a, b) {
+        return a + b;
     }
 
-    function subtract(a,b){
-        return a-b;
+    function subtract(a, b) {
+        return a - b;
     }
-    function multiply(a,b){
-        return a*b
+    function multiply(a, b) {
+        return a * b
     }
-    function divide(a,b){
-        return a/b;
+    function divide(a, b) {
+        return a / b;
     }
 
 
-    
-    
+
+
 })
