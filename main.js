764db4e6f55580732104f3e7d410bd6f87a3138b
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-03-10 19:33:44 -0500

    Added readme image

--- main.js
+++ main.js
@@ -70,7 +70,11 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
-        this.previousOperandTextElement.innerText = this.previousOperand;
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.previousOperand} ${this.operation}`
+        }
+        //this.previousOperandTextElement.innerText = this.previousOperand;
     }
 }
 
